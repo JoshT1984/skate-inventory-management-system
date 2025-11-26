a60866970c0283db2cedc7e62dbaef55d6ebb81a
xxblackhawkdevxx <xxblackhawkdevxx@mac.lan>
2025-11-26 16:36:01 -0600

    Initial Spring Boot project setup

---
+++ src/main/java/com/skillstorm/skate_inventory_mgmt_system/SkateInventoryMgmtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.skate_inventory_mgmt_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkateInventoryMgmtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkateInventoryMgmtSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillstorm/skate_inventory_mgmt_system/SkateInventoryMgmtSystemApplicationTests.java
@@ -0,0 +1,12 @@
+package com.skillstorm.skate_inventory_mgmt_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkateInventoryMgmtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
